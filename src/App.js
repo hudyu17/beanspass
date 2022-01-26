f7e3bbf305ea1f7a18269279e471b2ee5d81bf94
hudyu17 <hengjeung.yuen@gmail.com>
2022-01-25 21:18:22 -0500

    ga tracking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
- 
+import ReactGA from 'react-ga';
 import Home from './components/Home';
 
+function initializeReactGA() {
+  ReactGA.initialize('G-2R889R50LZ');
+  ReactGA.pageview('/');
+}
+
 function App() {
+  initializeReactGA();
   return (
       <div>
         <Home/>
