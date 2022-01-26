81ace56a1d18bfb86887fce2dd3436cd589b59f6
hudyu17 <hengjeung.yuen@gmail.com>
2022-01-25 21:29:27 -0500

    ga4 tracking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import Home from './components/Home';
 
 function initializeReactGA() {
   ReactGA.initialize('G-2R889R50LZ');
-  ReactGA.pageview('/');
+  ReactGA.send('Landing page view');
 }
 
 function App() {
